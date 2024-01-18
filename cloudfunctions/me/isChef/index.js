141800dedc0e044294f5425f052ffc3065b50151
xinxiang <221850120@smail.nju.edu.cn>
2024-01-18 23:57:48 +0800

    修改了一些语法错误

--- cloudfunctions/me/isChef/index.js
+++ cloudfunctions/me/isChef/index.js
@@ -6,29 +6,31 @@ const db = cloud.database();
 const chefCollection = db.collection("chefs");
 // 云函数入口函数
 exports.main = async (event, context) => {
-	const {newChefID}=event;
+  const { newChefID } = event;
   try {
-	const res=await chefCollection.where({
-		chefID=newChefID
-	}).get();
+    const res = await chefCollection
+      .where({
+        chefID: newChefID,
+      })
+      .get();
     if (res.data.length > 0) {
-      console.log("true")
+      console.log("true");
       return {
-		isValid: true,
-		errMsg:""
+        isValid: true,
+        errMsg: "",
       };
     } else {
-	  console.log("false");
-	  return{
-		  isValid:false,
-		  errMsg:""
-	  }
+      console.log("false");
+      return {
+        isValid: false,
+        errMsg: "",
+      };
     }
   } catch (err) {
-	  console.log(err)
-	  return {
-		  isValid:false,
-		  errMsg:"An unexpected incident occurred, please submit feedback."
-	  }
+    console.log(err);
+    return {
+      isValid: false,
+      errMsg: "An unexpected incident occurred, please submit feedback.",
+    };
   }
 };
