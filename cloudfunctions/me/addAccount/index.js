b47739bfd8bbd9576804ccdf4b93d9db631ec46e
xinxiang <221850120@smail.nju.edu.cn>
2024-01-20 19:55:05 +0800

    补充了在addAccount中增加厨师账户的功能

--- cloudfunctions/me/addAccount/index.js
+++ cloudfunctions/me/addAccount/index.js
@@ -5,6 +5,7 @@ cloud.init({
 const db = cloud.database();
 exports.main = async (event, context) => {
   try {
+    const isChef = event.data.isChef;
     let { OPENID, APPID, UNIONID } = cloud.getWXContext();
     const collection = await db.collection("users").get();
     const users = collection.data;
@@ -15,6 +16,9 @@ exports.main = async (event, context) => {
     newUser_id = "000000" + newUser_id;
     const idLength = newUser_id.length;
     newUser_id = newUser_id.substring(idLength - 6, idLength);
+    if (!isChef) {
+      newUser_id = "888" + newUser_id.substring(3, 6);
+    }
     db.collection("users").add({
       data: {
         nickname: "User" + newUser_id + "(默认)",
