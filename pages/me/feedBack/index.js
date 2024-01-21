73019c40fab5fe177b5475231d44b24eb6cf639f
xinxiang <221850120@smail.nju.edu.cn>
2024-01-21 11:12:50 +0800

    修改了前端feedback的调用

--- pages/me/feedBack/index.js
+++ pages/me/feedBack/index.js
@@ -1,92 +1,78 @@
-
 // pages/me/feedBack/index.js
 Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    hint: "欢迎在这里畅所欲言，说出你对小程序的需求、意见或建议！",
+    text: "",
+    isEmpty: true,
+    isSubmitted: false,
+    value: "",
+  },
 
-    /**
-     * 页面的初始数据
-     */
-    data:{
-      hint: '欢迎在这里畅所欲言，说出你对小程序的需求、意见或建议！',
-      text: '',
-      isEmpty: true,
-      isSubmitted: false,
-      value: "",
-    },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {},
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad(options) {
+  inputHandler(e) {
+    this.setData({
+      value: e.detail.value,
+      isEmpty: e.detail.value === "",
+    });
+  },
 
-    },
-
-    inputHandler(e) {
-      this.setData({
-        value: e.detail.value,
-        isEmpty: e.detail.value === '',
-      })
-    },
-
-    submit() {
-      //todo 向后端发送数据
-      wx.cloud.callFunction({
+  submit() {
+    //todo 向后端发送数据
+    wx.cloud
+      .callFunction({
         name: "me",
-        type: "sendFeedback",
-        feedbackContent: this.data.value,
-      }).then((res)=>{
-        console.log(res)
+        data: {
+          type: "sendFeedback",
+          feedbackContent: this.data.value,
+        },
       })
-      this.setData({
-        isSubmitted: true,
-      })
-    },
+      .then((res) => {
+        console.log(res);
+      });
+    this.setData({
+      isSubmitted: true,
+    });
+  },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {},
 
-    },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {},
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {},
 
-    },
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {},
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {},
 
-    },
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {},
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-
-    }
-})
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {},
+});
