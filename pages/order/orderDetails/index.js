8f682019edcc6d89e0b475a24bcd15cbff06f809
xinxiang <221850120@smail.nju.edu.cn>
2024-01-23 19:39:28 +0800

    ensure

--- pages/order/orderDetails/index.js
+++ pages/order/orderDetails/index.js
@@ -1,11 +1,7 @@
 // pages/order/orderDetails/index.js
-import {
-  readyImgPath,
-  unreadyImgPath
-} from "../../enumerations.js";
+import { readyImgPath, unreadyImgPath } from "../../enumerations.js";
 
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -45,107 +41,96 @@ Page({
    */
   onLoad(options) {
     const id = options.billID;
-    console.log(id)
-    wx.cloud.callFunction({
-      name: "order",
-      data: {
-        type: "inquireSpecificOrders",
-        orderNo: id,
-      }
-    }).then((res) => {
-      if (res.result.success) {
-
-        console.log(res);
-        var imgPath
-        const isFinish = res.result.order.Status;
-        var isChef;
-        const userID = res.result.order.User_id
-        if (userID.startsWith("888")) isChef = true;
-        else isChef = false;
-        if (isFinish) imgPath = readyImgPath
-        else imgPath = unreadyImgPath
-        const dishes = res.result.order.Dishes;
-        var previewLMRs = [];
-        for (var i = 0; i < dishes.length; i++) {
-          let newPre = {
-            index: i,
-            midText: dishes[i].Name + " ￥" + dishes[i].Price,
-            rightText: dishes[i].Num,
-            id: dishes[i].ID,
-            hasBtn: false,
-            conditionForDisplay: true,
-            imagePath: imgPath,
+    console.log(id);
+    wx.cloud
+      .callFunction({
+        name: "order",
+        data: {
+          type: "inquireSpecificOrders",
+          orderNo: id,
+        },
+      })
+      .then((res) => {
+        if (res.result.success) {
+          console.log(res);
+          var imgPath;
+          const isFinish = res.result.order.Status;
+          var isChef;
+          const userID = res.result.order.User_id;
+          if (userID.startsWith("888")) isChef = true;
+          else isChef = false;
+          if (isFinish) imgPath = readyImgPath;
+          else imgPath = unreadyImgPath;
+          const dishes = res.result.order.Dishes;
+          var previewLMRs = [];
+          for (var i = 0; i < dishes.length; i++) {
+            let newPre = {
+              index: i,
+              midText: dishes[i].Name + " ￥" + dishes[i].Price,
+              rightText: dishes[i].Num,
+              id: dishes[i].ID,
+              hasBtn: false,
+              conditionForDisplay: true,
+              imagePath: imgPath,
+            };
+            previewLMRs.push(newPre);
           }
-          previewLMRs.push(newPre)
+          this.setData({
+            isFinish: isFinish,
+            isChef: isChef,
+            previewLMRs: previewLMRs,
+            id: id,
+          });
         }
-        this.setData({
-          isFinish: isFinish,
-          isChef: isChef,
-          previewLMRs: previewLMRs,
-          id: id,
-        })
-
-      }
-    })
+      });
   },
 
   ensure() {
-    wx.cloud.callFunction({
-      name: "order",
-      data: {
-        No: this.data.id,
-      }
-    }).then((res) => {
-      console.log(res)
-    })
+    wx.cloud
+      .callFunction({
+        name: "order",
+        data: {
+          type: "ensure",
+          No: this.data.id,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+      });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
-
-  },
+  onReady() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {
-
-  },
+  onShow() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {
-
-  },
+  onHide() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() {
-
-  },
+  onUnload() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {
-
-  },
+  onPullDownRefresh() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {
-
-  },
+  onReachBottom() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {
-
-  }
-})
+  onShareAppMessage() {},
+});
